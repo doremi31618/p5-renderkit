9cbf10075b092eb8025cf5ca9264469a39b8804b
doremi31618 <doremi31618@gmail.com>
2023-07-16 11:46:11 +0800

    ver 1.0.1

---
+++ index.js
@@ -0,0 +1,9 @@
+import { ExperimentRenderKit,RenderKit  } from "./lib/renderkit/renderkit.js";
+
+//renderkit output
+export default ExperimentRenderKit;
+export const Renderkit = RenderKit;
+
+//utilities output
+
+//contents output

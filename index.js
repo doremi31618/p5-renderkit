856e0bfb7fea99e3a3bd71ca503e3a73546e26e3
doremi31618 <doremi31618@gmail.com>
2023-07-19 09:28:08 +0800

    unfinish sceneplayer script

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { ExperimentRenderKit,RenderKit  } from "./lib/renderkit/renderkit.js";
 
 //renderkit output
-export default ExperimentRenderKit;
+export const ExperimentRenderKit = ExperimentRenderKit;
 export const Renderkit = RenderKit;
 
 //utilities output
